74136eb591314279d3f0d92884e7d574808308af
Nanthabala Mariyappan <mnanthabala@pkglobal.com>
2020-06-30 23:18:25 +0530

    Add project files.

---
+++ PhonebookApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using PhonebookApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhonebookApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<UserData> Phonebook = new List<UserData>();
+        public ActionResult GoToHome()
+        {
+            return View("HomePage");
+        }
+
+        public ActionResult NewUser()
+        {
+            
+            ViewBag.Message = "Enter the User Details";
+
+            return View("UserDetails");
+        }
+       
+        public ActionResult Save(UserData user)
+        {
+            Phonebook.Add(user);
+            return View(Phonebook);
+
+        }
+    }
+}
